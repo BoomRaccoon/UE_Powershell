dff0a8dfaef111768df58cffd828bff99489b2ce
Vladislav Foerster <vladi_foerster@web.de>
2023-06-13 23:05:54 +0200

    Scripts to create packs
    To create the folder structure and basic files for the pack as well as generate the actual .upack file

---
+++ (UE)CreatePackStructure.ps1
@@ -0,0 +1,122 @@
+﻿# region 
+
+if($args.Count -eq 0)
+{
+	$Path_targetDir = Read-Host -Prompt "Give the directory the folders should be created in"
+	$Name_Pack = Read-Host -Prompt "Give the name the pack should be named"
+}
+elseif ($args.Count -eq 1)
+{
+	if(Test-Path $args[0])
+		{ $Path_targetDir = $args[0]+ "\" }
+	else
+	{
+		$Path_targetDir = Read-Host -Prompt "The given path was incorrect please give a valid path"
+		$Name_Pack = Read-Host -Prompt "Give the name the pack should be named"
+	}
+	$Name_Pack = Read-Host -Prompt "Give the pack a name"
+}
+elseif ($args.Count -eq 2) {
+	if(Test-Path $args[0])
+	{
+
+	}
+}
+
+Write-Host "Do you want to add a description? (y/n)"
+$selection = $Host.UI.RawUI.ReadKey()
+if($selection.Character -eq "y")
+{
+	$Description = Read-Host -Prompt "Please type in the description you want to use for the pack. (Can be changed afterwards inside manifest.json)"
+}
+
+Write-Host "Do you want to add search tags? (y/n)"
+$selection = $Host.UI.RawUI.ReadKey()
+if($selection.Character -eq "y")
+{
+	$Tags = Read-Host -Prompt "Type the search tags seperated with a comma like 'desert,landscape,net'. (Can be changed afterwards inside manifest.json)"
+}
+
+$Path_Full = $Path_targetDir + $Name_Pack
+
+# endregion
+
+
+# region StringsForFiles
+$ManifestContent = 
+@"
+{
+	"Version": 1,
+	"Name":
+	[
+		{
+			"Language": "en",
+			"Text": "$Name_Pack"
+		}
+	],
+	"Description":
+	[
+		{
+			"Language": "en",
+			"Text": "$(If ($Description) {$Description} Else {"You can change this description before packing in the manifest.json"})"
+
+		}
+	],
+	"AssetTypes": [],
+	"SearchTags":
+	[
+		{
+			"Language": "en",
+			"Text": "$(If ($Tags) {$Tags} Else {})"
+		}
+	],
+	"ClassTypes": "",
+	"Category": "Content",
+	"Thumbnail": "$($Name_Pack).png",
+	"Screenshots":
+	[
+		"$($Name_Pack)_Preview.png"
+	]
+}
+"@
+
+$ConfigContent =
+@"
+[AdditionalFilesToAdd]
++Files=Samples/$($Name_Pack)/Content/*.*
+"@
+
+$ContentToPack =
+@"
+"$Path_Full\ContentSettings\Config\"
+"$Path_Full\ContentSettings\Media\"
+"$Path_Full\ContentSettings\manifest.json"
+"@
+
+# endregion
+
+
+
+
+
+
+# region CreateDirectoriesAndFiles
+New-Item -ItemType Directory -Path $Path_Full
+New-Item -ItemType Directory -Path ($Path_Full + "\ContentSettings")
+New-Item -ItemType Directory -Path ($Path_Full + "\ContentSettings\Config")
+New-Item -ItemType Directory -Path ($Path_Full + "\ContentSettings\Media")
+New-Item -Path ($Path_Full + "\ContentSettings\Media\$($Name_Pack).png")
+New-Item -Path ($Path_Full + "\ContentSettings\Media\$($Name_Pack)_Preview.png")
+New-Item -Path ($Path_Full + "\ContentSettings\manifest.json")
+Set-Content ($Path_Full + "\ContentSettings\manifest.json")  $ManifestContent
+New-Item -Path ($Path_Full + "\ContentSettings\Config\config.ini")
+Set-Content ($Path_Full + "\ContentSettings\Config\config.ini") $ConfigContent
+
+New-Item -ItemType Directory -Path ($Path_Full + "\Samples")
+New-Item -ItemType Directory -Path ($Path_Full + "\Samples\$($Name_Pack)\Content")
+New-Item -ItemType Directory -Path ($Path_Full + "\FeaturePacks")
+New-Item -Path ($Path_Full + "\ContentToPack.txt")
+Set-Content ($Path_Full + "\ContentToPack.txt") $ContentToPack
+# endregion
+
+Write-Host "Now just add the actual content inside of Samples/Content/"
---
+++ (UE)GeneratePack.ps1
@@ -0,0 +1,43 @@
+﻿# region MAKE_UEPATH
+$LauncherInstallFile = $env:ProgramData + "\Epic\UnrealEngineLauncher\LauncherInstalled.dat"
+$bInstallerExists = Test-Path $LauncherInstallFile
+
+$args[0] = $args[0] -replace ("^'")  -replace ("'$")
+$File_ContentToPack = Join-Path -Path $args[0] -ChildPath "\ContentToPack.txt"
+$File_ContentToPack
+
+if ( ($bInstallerExists) ) {
+    $Regex_UE = "(?<=InstallLocation`": `").*UE_.*(?=\`")"
+    
+    
+    $FoundMatches = select-string -Pattern $Regex_UE $LauncherInstallFile | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value | Select-Object -Unique
+    
+    #foreach ($match in $FoundMatches) {
+    #    write-host "["$FoundMatches.IndexOf($match)"]" $match
+    #}
+    #$selection = Read-Host -Prompt 'Select the path to use'
+    #$Path_UE = $FoundMatches[$selection]
+    $Path_UE = $FoundMatches[0]
+}
+else
+{ write-host "EXIT.\nBecause: couldn't find installed unreal version and no path was provided" }
+# endregion
+
+
+if( !(Test-Path -Path $File_ContentToPack) )
+    {Write-Host "EXIT`nBecause:The given path $($args[0])  does not contain a 'ContentToPack.txt'."; EXIT}
+
+
+# Get Pack-Name from JSON file
+$File_Manifest = Get-Childitem -File -Recurse -ErrorAction Stop -Path $args[0] -Include *manifest.json* 
+$Name_Pack = Get-Content -Raw -Path $File_Manifest | ConvertFrom-Json
+$Name_Pack = $Name_Pack.Name.Text + ".upack"
+# build path for output .upack file
+$OutPath_PackFile = Join-Path -Path $args[0] -ChildPath "\FeaturePacks\" | Join-Path -ChildPath $Name_Pack
+
+$File_ContentToPack
+$OutPath_PackFile
+
+$cmd = Join-Path -Path $Path_UE -ChildPath "\Engine\Binaries\Win64\UnrealPak.exe"
+$arguments = "-Create=$File_ContentToPack", $OutPath_PackFile
+&  $cmd $arguments
