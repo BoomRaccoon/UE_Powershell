025058972743c6499ae0c6073b11d07e73ed773a
Vladislav Foerster <vladi_foerster@web.de>
2023-06-24 08:07:48 +0200

    build pack will now be added to your engine

--- (UE)GeneratePack.ps1
+++ (UE)GeneratePack.ps1
@@ -1,26 +1,27 @@
 ﻿# region MAKE_UEPATH
-$LauncherInstallFile = $env:ProgramData + "\Epic\UnrealEngineLauncher\LauncherInstalled.dat"
-$bInstallerExists = Test-Path $LauncherInstallFile
+    $LauncherInstallFile = $env:ProgramData + "\Epic\UnrealEngineLauncher\LauncherInstalled.dat"
+    $bInstallerExists = Test-Path $LauncherInstallFile
 
-$args[0] = $args[0] -replace ("^'")  -replace ("'$")
-$File_ContentToPack = Join-Path -Path $args[0] -ChildPath "\ContentToPack.txt"
-$File_ContentToPack
+    $args[0] = $args[0] -replace ("^'")  -replace ("'$")
+    $File_ContentToPack = Join-Path -Path $args[0] -ChildPath "\ContentToPack.txt"
+    $File_ContentToPack
 
-if ( ($bInstallerExists) ) {
-    $Regex_UE = "(?<=InstallLocation`": `").*UE_.*(?=\`")"
-    
-    
-    $FoundMatches = select-string -Pattern $Regex_UE $LauncherInstallFile | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value | Select-Object -Unique
-    
-    #foreach ($match in $FoundMatches) {
-    #    write-host "["$FoundMatches.IndexOf($match)"]" $match
-    #}
-    #$selection = Read-Host -Prompt 'Select the path to use'
-    #$Path_UE = $FoundMatches[$selection]
-    $Path_UE = $FoundMatches[0]
-}
-else
-{ write-host "EXIT.\nBecause: couldn't find installed unreal version and no path was provided" }
+    if ( ($bInstallerExists) ) {
+        
+        $Regex_UE = "(?<=InstallLocation`": `").*UE_.*(?=\`")"
+        
+        
+        $FoundMatches = select-string -Pattern $Regex_UE $LauncherInstallFile | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value | Select-Object -Unique
+        
+        #foreach ($match in $FoundMatches) {
+        #    write-host "["$FoundMatches.IndexOf($match)"]" $match
+        #}
+        #$selection = Read-Host -Prompt 'Select the path to use'
+        #$Path_UE = $FoundMatches[$selection]
+        $Path_UE = $FoundMatches[0]
+    }
+    else
+    { write-host "EXIT.\nBecause: couldn't find installed unreal version and no path was provided" }
 # endregion
 
 
@@ -41,3 +42,12 @@ $OutPath_PackFile
 $cmd = Join-Path -Path $Path_UE -ChildPath "\Engine\Binaries\Win64\UnrealPak.exe"
 $arguments = "-Create=$File_ContentToPack", $OutPath_PackFile
 &  $cmd $arguments
+
+$Path_EngineFeaturePacks = Join-Path -Path $Path_UE -ChildPath "\FeaturePacks\"
+$Path_EngineSamples = Join-Path -Path $Path_UE -ChildPath "\Samples\"
+$Path_PackFeaturePacks = Join-Path -Path $args[0] -ChildPath "\FeaturePacks\*"
+$Path_PackSamples = Join-Path -Path $args[0] -ChildPath "\Samples\*"
+
+
+copy-item -Path $Path_PackFeaturePacks -Destination $Path_EngineFeaturePacks -Recurse
+copy-Item $Path_PackSamples -Destination $Path_EngineSamples -Recurse
